d3b6c655a164c23fe82819513d63532b1298eff8
ptuton <ptuton@gmail.com>
2016-11-10 13:24:36 +1000

    update message from world to pete

--- src/main/java/com/example/TestController.java
+++ src/main/java/com/example/TestController.java
@@ -10,6 +10,6 @@ public class TestController
     @RequestMapping(value = "/helloworld", method = RequestMethod.GET)
     public String hello()
     {
-        return "hello world";
+        return "hello pete!";
     }
 }
